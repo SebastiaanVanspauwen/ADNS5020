053f94fe8affaffb289320b6534e95677e12cf70
Sebastiaan <vanspauwensebastiaan@gmail.com>
2020-03-27 18:52:47 +0100

    Get pixel data

--- ADNS5020/ADNS5020.ino
+++ ADNS5020/ADNS5020.ino
@@ -1,20 +1,16 @@
 // Source: http://www.wildcircuits.com/2013/03/optical-mouse-hacking.html
 // include the SPI library: 
 #include <SPI.h> 
-  
 // setup pins 
-int nReset = 9; //12 (Changed to 9 because using Arduino Nano)
-int nCS = 13; 
-  
+int nReset = 2; //12 (Changed to 9 because using Arduino Nano)
+int nCS = 3; 
 void setup(){ 
   //set pin I/O direction 
   pinMode (nReset, OUTPUT); 
   pinMode (nCS, OUTPUT); 
-   
   //put the device in reset not chip selected 
   digitalWrite(nReset,LOW); 
   digitalWrite(nCS,HIGH); 
-   
   //initialize SPI 
   SPI.begin(); 
   SPI.setDataMode(SPI_MODE3); 
@@ -23,33 +19,14 @@ void setup(){
   delay(1000); 
   Serial.println("startup complete"); 
 } 
-  
-void loop(){ 
-  unsigned int read_data; 
-   
-  delay(1000); 
+void loop()
+{ 
   //enable ADNS 
   digitalWrite(nReset,HIGH); 
-   
   //startup time 
   delay(1000); 
-   
-  //read and print a register map 
-  Serial.println("ADNS-5020 Register Map"); 
-  Serial.println("Addr 0x00 - Data " + String(ADNS_read(0x00))); 
-  Serial.println("Addr 0x01 - Data " + String(ADNS_read(0x01))); 
-  Serial.println("Addr 0x02 - Data " + String(ADNS_read(0x02))); 
-  Serial.println("Addr 0x03 - Data " + String(ADNS_read(0x03))); 
-  Serial.println("Addr 0x04 - Data " + String(ADNS_read(0x04))); 
-  Serial.println("Addr 0x05 - Data " + String(ADNS_read(0x05))); 
-  Serial.println("Addr 0x06 - Data " + String(ADNS_read(0x06))); 
-  Serial.println("Addr 0x07 - Data " + String(ADNS_read(0x07))); 
-  Serial.println("Addr 0x08 - Data " + String(ADNS_read(0x08))); 
-  Serial.println("Addr 0x09 - Data " + String(ADNS_read(0x09))); 
-  Serial.println("Addr 0x0A - Data " + String(ADNS_read(0x0A))); 
-  delay(1000);   
-} 
-  
+  pixel_grab(); 
+}
 void ADNS_write(unsigned int address, unsigned int data){ 
   // take the CS pin low to select the chip: 
   digitalWrite(nCS,LOW); 
@@ -58,19 +35,36 @@ void ADNS_write(unsigned int address, unsigned int data){
   SPI.transfer(data); 
   // take the SS pin high to de-select the chip: 
   digitalWrite(nCS,HIGH);  
-} 
-  
+}
 unsigned int ADNS_read(unsigned int address){ 
   unsigned int data; 
   // take the CS pin low to select the chip: 
   digitalWrite(nCS,LOW); 
   //  send in the address and value via SPI: 
   SPI.transfer(address); 
-  
   pinMode(MOSI, INPUT); 
   data = SPI.transfer(0x00); 
   pinMode(MOSI, OUTPUT); 
   // take the SS pin high to de-select the chip: 
   digitalWrite(nCS,HIGH);  
   return(data); 
+} 
+
+void pixel_grab(){ 
+  //address = 0x0b 
+  int xcount=0; 
+  int count=0; 
+   
+  //reset the pixel grab counter 
+  ADNS_write(0x0B,0x00);
+  Serial.println("");   
+  for (count=0;count < 225; count++){ 
+    if (count%15 == 14){ 
+      Serial.println(String(ADNS_read(0x0B))); 
+    } 
+    else{ 
+      Serial.print(String(ADNS_read(0x0B)) + ","); 
+    } 
+  } 
+  Serial.println(""); 
 } 
